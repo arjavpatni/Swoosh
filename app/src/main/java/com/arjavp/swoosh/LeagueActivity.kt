8a52a9fadf163a768fe3066713d2168a25508a37
Arjav <arjavpatni@hotmail.com>
2020-01-09 20:01:30 +0530

    first layout

---
+++ app/src/main/java/com/arjavp/swoosh/LeagueActivity.kt
@@ -0,0 +1,12 @@
+package com.arjavp.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
